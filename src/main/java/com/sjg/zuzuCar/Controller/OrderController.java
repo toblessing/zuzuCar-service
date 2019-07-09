5940d932d571babb219df39cba0e2529d129dd55
toblessing <toblessing@163.com>
2019-07-10 00:50:38 +0800

    增加：取消预约单API

--- src/main/java/com/sjg/zuzuCar/Controller/OrderController.java
+++ src/main/java/com/sjg/zuzuCar/Controller/OrderController.java
@@ -83,6 +83,35 @@ public class OrderController {
         }
         return message;
     }
+    @PostMapping("cancelOrder")
+    public Message<?> cancelOrder(@RequestBody ReservationForm order) {
+
+        Message<List> message = new Message<List>();
+        Account account = Tools.getUserFromSeesion();
+
+        if (account == null) {
+            message.setSuccess(false);
+            message.setMsg("您尚未登录，请登录后再试！");
+            return message;
+        }
+        ReservationForm reservationForm = orderMapper.selectByPrimaryKey(order.getReservationFormId());
+        if(reservationForm.getReservationFormId()!=order.getReservationFormId()){
+            message.setSuccess(false);
+            message.setMsg("这不是您的预约单，请检查后重试");
+            return message;
+        }
+
+        reservationForm.setReservationId(0);
+        reservationForm.setReservationTime(0L);
+        if (reservationFormService.update(reservationForm)!=0){
+            message.setSuccess(true);
+            message.setMsg("取消成功");
+        }else {
+            message.setSuccess(false);
+            message.setMsg("取消失败");
+        }
+        return message;
+    }
 
 
     @GetMapping("readFreeOrder")
